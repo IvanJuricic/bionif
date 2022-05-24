68dcde4616a35026a06467ec2347f081d487cf37
Ivan Juricic <ivanjuricic97@gmail.com>
2022-05-24 05:03:06 +0200

    Add search and insert

--- main.c
+++ main.c
@@ -7,8 +7,7 @@
 
 // Structs
 typedef struct {
-    unsigned int key;
-    char *value;
+    unsigned int key, value;
 } HashTableItem;
 
 typedef struct {
@@ -20,15 +19,24 @@ typedef struct {
 // Function declarations
 char* append_string(char *s1, char *s2);
 unsigned int create_fingerprint(unsigned char *s);
+unsigned int hash1(unsigned int x);
+HashTableItem* create_hash_item(unsigned int key, unsigned int value);
+HashTable* create_hash_table(int size);
+void insert_sequence_hash_to_table(HashTable* table, char* sequence);
 
-HashTableItem add_hash_item(unsigned int key, char *value);
+void free_item(HashTableItem* item);
+void free_table(HashTable* table);
+
 
 int main() {
 
     FILE *fp;
-    int entries = 0, seqHash;
+    int entries = 0, seqHash, idx;
     char buff[BUFF_LEN], *sequence, *tmp;
     bool firstEntry = true;
+
+    HashTable *hashTable;
+    hashTable = create_hash_table(200);
 
     fp = fopen("data", "r");
 
@@ -39,8 +47,11 @@ int main() {
         }
         else if(sequence != NULL && buff[0] == '>' && firstEntry == false) {
             // Add sequence to cuckoo filter
-            seqHash = hash_func(sequence);
-            printf("Sekvenca %d => len: %ld, hash: %u\n", entries, strlen(sequence), seqHash);
+            insert_sequence_hash_to_table(hashTable, sequence);
+            //seqHash = create_fingerprint(sequence);
+            //idx = hash1(seqHash);
+            //printf("Sekvenca %d => len: %ld, hash: %u\n", entries, strlen(sequence), seqHash);
+            printf("Sekvenca %d => hash: %d\n", entries, idx);
             sequence = NULL;
             entries++;
             continue;
@@ -75,14 +86,68 @@ HashTable* create_hash_table(int size) {
 }
 
 // Create hash item from hashed sequence 
-HashTableItem* create_hash_item(unsigned int key, char *value) {
+HashTableItem* create_hash_item(unsigned int key, unsigned int value) {
     HashTableItem *item = (HashTableItem *) malloc(sizeof(HashTableItem));
     
     item -> key = key;
-    item -> value = (char *) malloc(strlen(value) + 1);
-    strcpy(item -> value, value);
-
+    item -> value = value;
+    
     return item;
+}
+
+void* search_for_item(HashTable* table, char* sequence) {
+    int seqHash = create_fingerprint(sequence);
+    int idx = hash1(seqHash);
+
+    HashTableItem* item = table->items[idx];
+ 
+    // Ensure that we move to a non NULL item
+    if (item != NULL) {
+        if (item->value == seqHash)
+            printf("Sequence found!\n");
+    }
+    printf("Sequence NOT found!\n");
+}
+
+// Add item to table
+void insert_sequence_hash_to_table(HashTable* table, char* sequence) {
+    // Create the item
+    int seqHash = create_fingerprint(sequence);
+    int idx = hash1(seqHash);
+
+    HashTableItem* item = create_item(idx, seqHash);
+    HashTableItem* current_item = table->items[idx];
+    
+    if (current_item == NULL) {
+        // Key does not exist.
+        if (table -> count == table -> size) {
+            // Hash Table Full
+            printf("Insert Error: Hash Table is full\n");
+            free_item(item);
+            return;
+        }
+        
+        // Insert directly
+        table->items[idx] = item; 
+        table->count++;
+    }
+}
+
+// Free hash table item
+void free_item(HashTableItem* item) {
+    free(item);
+}
+
+// Free hash table
+void free_table(HashTable* table) {
+    for (int i = 0; i < table -> size; i++) {
+        HashTableItem* item = table -> items[i];
+        if (item != NULL)
+            free_item(item);
+    }
+ 
+    free(table->items);
+    free(table);
 }
 
 // Sequence fingerprint creation
@@ -93,6 +158,14 @@ unsigned int create_fingerprint(unsigned char *s) {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
     return hash;
+}
+
+// Hash table function
+unsigned int hash1(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
 }
 
 // Append DNA sequences if longer than buffer limit
