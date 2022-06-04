63ede24426af1d7b1a5e51165ae43ab6dd0dd3f7
Ivan Juricic <ivanjuricic97@gmail.com>
2022-06-05 00:42:12 +0200

    create test sequences

--- generator.c
+++ generator.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFF_SIZE 50
+int BUFF_SIZE = 0;
 
 // Random sequence generator
 static char *rand_string(char *str, size_t size)
@@ -21,7 +21,7 @@ static char *rand_string(char *str, size_t size)
 
 int main(int argc, char *argv[]) {
 
-    FILE *original, *generated, *tmp;
+    FILE *original, *generated_10, *generated_20, *generated_50, *generated_100, *generated_1000, *tmp;
 
     if(argc != 2) {
         printf("\n\tUsage: ./test.o filename\n");
@@ -29,43 +29,79 @@ int main(int argc, char *argv[]) {
     }
 
     char *filename = argv[argc-1];
+
     char *tmp_filename = "tmp";
-    char *gen_filename = "test_seq_50";
-    char buff[BUFF_SIZE + 1];
 
-    original = fopen(filename, "r");
-    generated = fopen(gen_filename, "w+");
-    tmp = fopen(tmp_filename, "w+");
+    char *gen_filename_10 = "test_seq_10";
+    char *gen_filename_20 = "test_seq_20";
+    char *gen_filename_50 = "test_seq_50";
+    char *gen_filename_100 = "test_seq_100";
+    char *gen_filename_1000 = "test_seq_1000";
 
-    while(fgets(buff, BUFF_SIZE + 1, (FILE *)original) != NULL) {
-        //printf("%s\n", buff);
-        if(strlen(buff) == BUFF_SIZE) {
-            //printf("strlen buff: %ld, buff size: %d\n", strlen(buff), BUFF_SIZE);
-            fprintf(tmp, "%s\n", buff);
-        }
-    }
-
-    char *rand_sequence = malloc(sizeof(char) * BUFF_SIZE);
-
-    for(int i = 0; i < 1000; i++) {
-        tmp = fopen(tmp_filename, "r");
-
-        rand_string(rand_sequence, BUFF_SIZE);
+    for(int j = 0; j < 5; j++) {
         
-        while(fgets(buff, BUFF_SIZE + 1, (FILE *)tmp) != NULL) {
-            if(strlen(buff) == BUFF_SIZE && strcmp(buff, rand_sequence) != 0) {
-                //printf("Rand seq => %ld\n", strlen(rand_sequence));
-                fprintf(generated, "%s\n", rand_sequence);
-                break;
+        char *gen_file_name;
+        FILE *gen_file;
+
+        if(j == 0) {
+            BUFF_SIZE = 10;
+            gen_file_name = gen_filename_10;
+        }
+        else if(j == 1) {
+            BUFF_SIZE = 20;
+            gen_file_name = gen_filename_20;
+        }
+        else if(j == 2) {
+            BUFF_SIZE = 50;
+            gen_file_name = gen_filename_50;
+        }
+        else if(j == 3) {
+            printf("Tu smo\n");
+            BUFF_SIZE = 100;
+            gen_file_name = gen_filename_100;
+        }
+        else if(j == 4) {
+            BUFF_SIZE = 1000;
+            gen_file_name = gen_filename_1000;
+        }
+
+        char buff[BUFF_SIZE + 1];
+
+        original = fopen(filename, "r");
+        tmp = fopen(tmp_filename, "w+");
+        gen_file = fopen(gen_file_name, "w+");
+
+        while(fgets(buff, BUFF_SIZE + 1, (FILE *)original) != NULL) {
+            //printf("%s\n", buff);
+            if(strlen(buff) == BUFF_SIZE) {
+                //printf("strlen buff: %ld, buff size: %d\n", strlen(buff), BUFF_SIZE);
+                fprintf(tmp, "%s\n", buff);
             }
         }
+
+        char *rand_sequence = malloc(sizeof(char) * BUFF_SIZE);
+
+        for(int i = 0; i < 5000; i++) {
+            
+            tmp = fopen(tmp_filename, "r");
+            rand_string(rand_sequence, BUFF_SIZE);
+            
+            while(fgets(buff, BUFF_SIZE + 1, (FILE *)tmp) != NULL) {
+                if(strlen(buff) == BUFF_SIZE && strcmp(buff, rand_sequence) != 0) {
+                    fprintf(gen_file, "%s\n", rand_sequence);
+                    break;
+                }
+            }
+
+            fclose(tmp);
+        }
+
+        fclose(gen_file);
+        free(rand_sequence);
+        rand_sequence = NULL;
     }
 
-    free(rand_sequence);
-    rand_sequence = NULL;
-
-    fclose(generated);
-    fclose(tmp);
+    printf("Exiting!\n");
 
     return 0;
 }
